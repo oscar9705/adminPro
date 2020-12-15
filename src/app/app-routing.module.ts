2a7c22d9b89f999e76f9ec51fee2da5e73215bcf
oscar salazar <salazaroscar9705@gmail.com>
2020-12-14 21:57:17 -0500

    practica de una plantilla al framework

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagesComponent } from './pages/pages.component';
+ 
+
+const routes: Routes = [
+    
+    {
+        path: '', 
+        component: PagesComponent,
+        children:[
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'grafica1', component: Grafica1Component },
+            { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+
+        ]
+    }, 
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    { path: '**', component: NopagefoundComponent }
+]
+@NgModule({
+    declarations:[],
+    imports:[
+        RouterModule.forRoot(routes)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,18 @@
+import { LoginComponent } from './../../auth/login/login.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.css']
+})
+export class NopagefoundComponent {
+  
+ 
+   year = new Date().getFullYear();    
+   
+   
+  
+  
+
+}
